fa898dce6d995832f295c1945116c7ec89881505
trion129 <parmsingh129@yahoo.com>
2016-12-04 21:06:36 +0530

    no space inside

--- Tools/UsernameGenerator/js/main.js
+++ Tools/UsernameGenerator/js/main.js
@@ -1,9 +1,12 @@
+$(".generated").hide();
+
 $("#generate").on('click',function(){
   var value = $('#sentence').val();
   
   
   $.getJSON('http://c10963cc.ngrok.io/name/'+ value, function(data){
     $('.generated').html('<p>'+data.data.name+'</p>');
+    $(".generated").show();
   });
   
 });
